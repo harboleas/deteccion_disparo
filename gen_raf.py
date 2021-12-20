5322d76ccbc8580f66aa9f13a7d130807c16d2b0
Hugo Arboleas <harboleas@citedef.gob.ar>
2021-12-20 09:34:44 -0300

    modif. gen_raf

--- gen_raf.py
+++ gen_raf.py
@@ -5,10 +5,34 @@ def read_csv(file_name, cap_resol):
     "Abre archivos creados con osci."
     f = open(file_name, "r")
     lines = f.readlines()
+    n = int(lines[0].split(",")[1])
+    T = float(lines[10].split(",")[1])
+    escala_V = float(lines[4].split(",")[1])
+    V_unit = lines[3].split(",")[1]
+    T_unit = lines[6].split(",")[1]
+    print("Tiempo total:", n*T, T_unit)
+    print("vmin:", -escala_V * 5, V_unit)
+    print("Vmax:", escala_V * 5, V_unit)
     datos = np.array([int(x) for x in lines[13:-1]])
-    # desplazo los datos y hago la conversion a 16 bits
+    # desplazo los datos, para obtener solo valores positivos
+    # y luego hago la conversion de resolucion a 14 bits
     offset = 2**(cap_resol - 1)
     return (datos + offset) * 2**(14-cap_resol)
+
+
+def recorta(datos, val_min, val_max):
+
+    datos_proc = []
+    for val in datos:
+        if val <= val_min:
+           datos_proc.append(val_min)
+        elif val >= val_max:
+            datos_proc.append(val_max)
+        else:
+            datos_proc.append(val)
+
+    return np.array(datos_proc)
+
 
 def gen_raf(file_name, datos):
     # interpolacion lineal de los datos para alcanzar los 2**14
